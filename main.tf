060297d393469f377e16b9184676e63ee567be55
tahashinegp <tahashinaws@outlook.com>
2022-03-21 01:35:15 +0600

    added main.tf

--- main.tf
+++ main.tf
@@ -14,25 +14,16 @@
 # in backend.tf.
 
 
-resource "fakewebservices_vpc" "primary_vpc" {
-  name       = "Primary VPC"
-  cidr_block = "0.0.0.0/1"
+resource "aws_s3_bucket" "test" {
+  bucket = "abhitahaa-tf-test-bucket"
+
+  tags = {
+    Name        = "My bucket"
+    Environment = "abc"
+  }
 }
 
-resource "fakewebservices_server" "servers" {
-  count = 2
-
-  name = "Server ${count.index + 1}"
-  type = "t2.micro"
-  vpc  = fakewebservices_vpc.primary_vpc.name
-}
-
-resource "fakewebservices_load_balancer" "primary_lb" {
-  name    = "Primary Load Balancer"
-  servers = fakewebservices_server.servers[*].name
-}
-
-resource "fakewebservices_database" "prod_db" {
-  name = "Production DB"
-  size = 256
+resource "aws_s3_bucket_acl" "example" {
+  bucket = aws_s3_bucket.test.id
+  acl    = "private"
 }
