4e93c8ba953a29e08600d6ac677d9f2a151786e4
tahashinegp <tahashinaws@outlook.com>
2022-03-21 01:27:42 +0600

    modified git ignore and backend

--- backend.tf
+++ backend.tf
@@ -1,13 +1,13 @@
 # The block below configures Terraform to use the 'remote' backend with Terraform Cloud.
 # For more information, see https://www.terraform.io/docs/backends/types/remote.html
-terraform {
-  backend "remote" {
-    organization = "abhitahaa"
+#terraform {
+ # backend "remote" {
+  #  organization = "abhitahaa"
 
-    workspaces {
-      name = "mlops"
-    }
-  }
+   # workspaces {
+     # name = "mlops"
+    #}
+  #}
 
-  required_version = ">= 0.13.0"
-}
+  #required_version = ">= 0.13.0"
+#}
