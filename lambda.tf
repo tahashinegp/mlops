02cc1188fb9c36678eadccb9d7cf9c3932a53b7a
abhitahaa <96439641+abhitahaa@users.noreply.github.com>
2022-03-23 01:37:19 +0600

    Update lambda.tf

--- lambda.tf
+++ lambda.tf
@@ -1,7 +1,7 @@
 
 resource "aws_lambda_function" "terraform_lambda_func" {
 function_name                  = "Automate_next_Lambda_Function"
-role                           = aws_iam_role.lambda_role_output
+role                           = aws_iam_role.lambda_role.arn
 handler                        = "index.lambda_handler"
 runtime                        = "python3.8"
 }
