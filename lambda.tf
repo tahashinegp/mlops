57ba69ac060567677d429e1c841601dd8b7d7cbc
abhitahaa <96439641+abhitahaa@users.noreply.github.com>
2022-03-23 02:27:06 +0600

    Update lambda.tf

--- lambda.tf
+++ lambda.tf
@@ -4,7 +4,7 @@ source_dir  = "${path.module}/test.py"
 output_path = "${path.module}/lambda.zip"
 }
 resource "aws_lambda_function" "terraform_lambda_func" {
-  filename                       = "${path.module}/lambda.zip""
+  filename                       = "${path.module}/lambda.zip"
   function_name                  = "Automate_next_Lambda_Function"
   role                           = aws_iam_role.lambda_role.arn
   handler                        = "index.lambda_handler"
