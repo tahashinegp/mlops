52b172c60a9d1d5204c1ec18ca65f78230023a44
abhitahaa <96439641+abhitahaa@users.noreply.github.com>
2022-03-23 01:35:02 +0600

    Update iam.tf

--- iam.tf
+++ iam.tf
@@ -45,8 +45,3 @@ resource "aws_iam_role_policy_attachment" "attach_iam_policy_to_iam_role" {
  role        = aws_iam_role.lambda_role.name
  policy_arn  = aws_iam_policy.iam_policy_for_lambda.arn
 }
-
-output "lambda_role_output" {
-  description = "ARN of admin IAM role"
-  value       = aws_iam_role.lambda_role.arn
-}
