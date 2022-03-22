70996fc64ba41d96cc5f062a394ac06dad619f6c
abhitahaa <96439641+abhitahaa@users.noreply.github.com>
2022-03-23 02:09:16 +0600

    Update lambda.tf

--- lambda.tf
+++ lambda.tf
@@ -2,7 +2,31 @@
 
 resource "local_file" "lambda_function" {
   content = templatefile("${path.module}/test1.py.tpl", {
-    region    = var.region
+    import json
+    import boto3
+
+    def lambda_handler(event,context):
+
+        entity_id = event['entity_id']
+        contact_id = event['contact_id']
+        s3 = boto3.resource('s3')
+
+        content_object = s3.Object('au-in-da', f'fin-j/en/{env}.json')
+
+        file_content = content_object.get()['Body'].read().decode('utf-8')
+        json_content = json.loads(file_content)
+
+        prob_response = {contact_id : json_content[contact_id]}
+
+        response[entity_id] = prob_resonse
+        response['message'] = 'Hello from "
+        responseObject = {}
+        responseObject ['statusCode']= 200
+        responseObject ['headers'] = {}
+        responseObject ['headers'] ['Content-Type'] = 'application/json'
+        responseObject ['body'] = json.dumps(response)
+
+        return responseObject
   })
   filename =  "${path.module}/test1.py"
 
