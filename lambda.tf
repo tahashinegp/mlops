f3fab310c19effd5e2581aa55307bc554af611f3
abhitahaa <96439641+abhitahaa@users.noreply.github.com>
2022-03-23 02:01:31 +0600

    Update lambda.tf

--- lambda.tf
+++ lambda.tf
@@ -1,8 +1,19 @@
 
+
+resource "local_file" "lambda_function" {
+  content = templatefile("${path.module}/test1.py.tpl", {
+    region    = var.region
+  })
+  filename =  "${path.module}/test1.py"
+
+  file_permission      = 0644
+  directory_permission = 0755
+}
+
 data "archive_file" "zip_the_python_code" {
   type        = "zip"
-  source_dir  = "${path.module}/python/"
-  output_path = "${path.module}/python/lambda.zip"
+  source_dir  = "${path.module}/"
+  output_path = "${path.module}/lambda.zip"
 }
 
 resource "aws_lambda_function" "terraform_lambda_func" {
