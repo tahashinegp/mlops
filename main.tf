acb4504106f4da72abbd3abb988266d73a24d5af
abhitahaa <96439641+abhitahaa@users.noreply.github.com>
2022-03-21 20:03:09 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,12 +1,12 @@
 #variable "env" {}
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 3.0"
-    }
-  }
-}
+# terraform {
+#   required_providers {
+#     aws = {
+#       source  = "hashicorp/aws"
+#       version = "~> 3.0"
+#     }
+#   }
+# }
 
 provider "aws" {
   region = "us-east-1"
