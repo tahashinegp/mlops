5117a3b24f859959e54781529e6ab0b8a73996ff
abhitahaa <96439641+abhitahaa@users.noreply.github.com>
2022-03-24 00:40:25 +0600

    Update apigateway.tf

--- apigateway.tf
+++ apigateway.tf
@@ -19,11 +19,19 @@ resource "aws_api_gateway_method" "method" {
     "method.request.path.proxy" = true
   }
 }
-
+resource "aws_api_gateway_method" "postmethod" {
+  rest_api_id   = "${aws_api_gateway_rest_api.api.id}"
+  resource_id   = "${aws_api_gateway_resource.resource.id}"
+  http_method   = "POST"
+  authorization = "NONE"
+  request_parameters = {
+    "method.request.path.proxy" = true
+  }
+}
 resource "aws_api_gateway_integration" "lambda_test" {
   rest_api_id = "${aws_api_gateway_rest_api.api.id}"
   resource_id = "${aws_api_gateway_resource.resource.id}"
-  http_method = "${aws_api_gateway_method.method.http_method}"
+  http_method = "${aws_api_gateway_method.postmethod.http_method}"
   integration_http_method = "POST"
   type                    = "AWS_PROXY"
   uri                     = aws_lambda_function.terraform_lambda_func.invoke_arn
