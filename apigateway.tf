b48cf2bd0ac1e557c195d375648644d04aab2858
abhitahaa <96439641+abhitahaa@users.noreply.github.com>
2022-03-23 15:16:57 -0400

    Update apigateway.tf

--- apigateway.tf
+++ apigateway.tf
@@ -11,16 +11,16 @@ resource "aws_api_gateway_rest_api" "api" {
 resource "aws_api_gateway_resource" "resource" {
   rest_api_id = "${aws_api_gateway_rest_api.api.id}"
   parent_id   = "${aws_api_gateway_rest_api.api.root_resource_id}"
-  path_part   = "{ProbPred_AN}"
+  path_part   = "ProbPred_AN"
 }
-resource "aws_api_gateway_method" "method" {
-  rest_api_id   = "${aws_api_gateway_rest_api.api.id}"
-  resource_id   = "${aws_api_gateway_resource.resource.id}"
-  http_method   = "ANY"
-  authorization = "NONE"
-  request_parameters = {
-    "method.request.path.proxy" = true
-  }
+# resource "aws_api_gateway_method" "method" {
+#   rest_api_id   = "${aws_api_gateway_rest_api.api.id}"
+#   resource_id   = "${aws_api_gateway_resource.resource.id}"
+#   http_method   = "ANY"
+#   authorization = "NONE"
+#   request_parameters = {
+#     "method.request.path.proxy" = true
+#   }
 }
 resource "aws_api_gateway_method" "postmethod" {
   rest_api_id   = "${aws_api_gateway_rest_api.api.id}"
