f243050d7830d2d125851ecb20bfed717038d8f3
abhitahaa <96439641+abhitahaa@users.noreply.github.com>
2022-03-23 13:03:33 -0400

    Update apigateway.tf

--- apigateway.tf
+++ apigateway.tf
@@ -4,6 +4,41 @@ resource "aws_api_gateway_rest_api" "api" {
  description = "Automate next API Gateway"
 }
 
+
+resource "aws_api_gateway_resource" "resource" {
+  rest_api_id = "${aws_api_gateway_rest_api.api.id}"
+  parent_id   = "${aws_api_gateway_rest_api.api.root_resource_id}"
+  path_part   = "{proxy+}"
+}
+resource "aws_api_gateway_method" "method" {
+  rest_api_id   = "${aws_api_gateway_rest_api.api.id}"
+  resource_id   = "${aws_api_gateway_resource.resource.id}"
+  http_method   = "ANY"
+  authorization = "NONE"
+  request_parameters = {
+    "method.request.path.proxy" = true
+  }
+}
+
+resource "aws_api_gateway_integration" "lambda" {
+  rest_api_id = "${aws_api_gateway_rest_api.api.id}"
+  resource_id = "${aws_api_gateway_resource.resource.id}"
+  http_method = "${aws_api_gateway_method.method.http_method}"
+  integration_http_method = "POST"
+  type                    = "AWS_PROXY"
+  uri                     = "{$aws_lambda_function.terraform_lambda_func.invoke_arn}"
+   
+}
+
+
+
+
+
+
+
+
+
+
 # resource "aws_api_gateway_authorizer" "demo" {
 #   name                   = "demo"
 #   rest_api_id            = aws_api_gateway_rest_api.demo.id
