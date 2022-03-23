757170fc393085291d650faee1a60d970792d933
abhitahaa <96439641+abhitahaa@users.noreply.github.com>
2022-03-23 15:18:27 -0400

    Update apigateway.tf

--- apigateway.tf
+++ apigateway.tf
@@ -21,7 +21,7 @@ resource "aws_api_gateway_resource" "resource" {
 #   request_parameters = {
 #     "method.request.path.proxy" = true
 #   }
-}
+#}
 resource "aws_api_gateway_method" "postmethod" {
   rest_api_id   = "${aws_api_gateway_rest_api.api.id}"
   resource_id   = "${aws_api_gateway_resource.resource.id}"
