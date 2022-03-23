b32206ef9b15b032d9c45fb770e9dab1f49fc43f
abhitahaa <96439641+abhitahaa@users.noreply.github.com>
2022-03-24 00:31:42 +0600

    Update apigateway.tf

--- apigateway.tf
+++ apigateway.tf
@@ -20,7 +20,7 @@ resource "aws_api_gateway_method" "method" {
   }
 }
 
-resource "aws_api_gateway_integration" "lambda" {
+resource "aws_api_gateway_integration" "lambda_test" {
   rest_api_id = "${aws_api_gateway_rest_api.api.id}"
   resource_id = "${aws_api_gateway_resource.resource.id}"
   http_method = "${aws_api_gateway_method.method.http_method}"
