991bcf7aa9ba32ecd3f8f368ba2383b6412f3818
abhitahaa <96439641+abhitahaa@users.noreply.github.com>
2022-03-24 00:36:43 +0600

    Update apigateway.tf

--- apigateway.tf
+++ apigateway.tf
@@ -26,7 +26,7 @@ resource "aws_api_gateway_integration" "lambda_test" {
   http_method = "${aws_api_gateway_method.method.http_method}"
   integration_http_method = "POST"
   type                    = "AWS_PROXY"
-  uri                     = "{$aws_lambda_function.terraform_lambda_func.invoke_arn}"
+  uri                     = aws_lambda_function.terraform_lambda_func.invoke_arn
    
 }
 
