f2b4574eeff3dba95353c25146178b9ed5a47d4d
abhitahaa <96439641+abhitahaa@users.noreply.github.com>
2022-03-23 15:04:04 -0400

    Update apigateway.tf

--- apigateway.tf
+++ apigateway.tf
@@ -11,7 +11,7 @@ resource "aws_api_gateway_rest_api" "api" {
 resource "aws_api_gateway_resource" "resource" {
   rest_api_id = "${aws_api_gateway_rest_api.api.id}"
   parent_id   = "${aws_api_gateway_rest_api.api.root_resource_id}"
-  path_part   = "{proxy+}"
+  path_part   = "{ProbPred_AN}"
 }
 resource "aws_api_gateway_method" "method" {
   rest_api_id   = "${aws_api_gateway_rest_api.api.id}"
@@ -30,7 +30,17 @@ resource "aws_api_gateway_method" "postmethod" {
   request_parameters = {
     "method.request.path.proxy" = true
   }
-}
+ }
+
+resource "aws_api_gateway_method" "gettmethod" {
+  rest_api_id   = "${aws_api_gateway_rest_api.api.id}"
+  resource_id   = "${aws_api_gateway_resource.resource.id}"
+  http_method   = "GET"
+  authorization = "NONE"
+  request_parameters = {
+    "method.request.path.proxy" = true
+  }
+
 resource "aws_api_gateway_integration" "lambda_test" {
   rest_api_id = "${aws_api_gateway_rest_api.api.id}"
   resource_id = "${aws_api_gateway_resource.resource.id}"
@@ -41,7 +51,15 @@ resource "aws_api_gateway_integration" "lambda_test" {
    
 }
 
-
+resource "aws_api_gateway_integration" "lambda_test" {
+  rest_api_id = "${aws_api_gateway_rest_api.api.id}"
+  resource_id = "${aws_api_gateway_resource.resource.id}"
+  http_method = "${aws_api_gateway_method.postmethod.http_method}"
+  integration_http_method = "GET"
+  type                    = "AWS_PROXY"
+  uri                     = aws_lambda_function.terraform_lambda_func.invoke_arn
+   
+}
 
 
 
