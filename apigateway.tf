327ce8d403e8fd385401a99f72adc0993a78ea36
abhitahaa <96439641+abhitahaa@users.noreply.github.com>
2022-03-24 15:03:35 -0400

    Update apigateway.tf

--- apigateway.tf
+++ apigateway.tf
@@ -85,10 +85,10 @@ resource "aws_api_gateway_deployment" "api_deployment" {
  resource "aws_lambda_permission" "apigw" {
   statement_id = "AllowAPIGatewayInvoke"
   action = "lambda:InvokeFunction"
-  function_name = "{aws_lambda_function.terraform_lambda_func.function_name}"
+  function_name = "aws_lambda_function.terraform_lambda_func.function_name"
   principal = "apigateway.amazonaws.com"
   
-  source_arn = "$aws_api_gateway_rest_api.api.execution_arn}/*/*"
+  source_arn = "${aws_api_gateway_rest_api.api.execution_arn}/*/*"
 
  }
 
