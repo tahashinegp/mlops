398ff5569885bf2d525b4926320aa233cf108603
abhitahaa <96439641+abhitahaa@users.noreply.github.com>
2022-03-25 21:40:52 -0400

    Update cognito.tf

--- cognito.tf
+++ cognito.tf
@@ -30,3 +30,13 @@ resource "aws_cognito_user" "example" {
     email_verified = true
   }
 }
+
+
+resource "aws_cognito_user_pool_client" "client" {
+  name = "client"
+
+  user_pool_id = aws_cognito_user_pool.pool.id
+
+  generate_secret     = true
+  explicit_auth_flows = ["ADMIN_NO_SRP_AUTH"]
+}
